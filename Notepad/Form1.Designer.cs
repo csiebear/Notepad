c790ba769a7e44bee38193db925c4928a72544d2
csiebearinmm <ching93011@gmail.com>
2017-04-16 20:15:48 +0800

    2017 04 16 C# Homework1
    First commit. Just design the UI form and add some rtf

---
+++ Notepad/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Notepad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.開啟新檔ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開啟舊檔ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.儲存檔案SToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.另存新檔ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.版面設定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.列印ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.結束XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripDropDownButton2 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.toolStripDropDownButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(552, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開啟新檔ToolStripMenuItem,
+            this.開啟舊檔ToolStripMenuItem,
+            this.儲存檔案SToolStripMenuItem,
+            this.另存新檔ToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.版面設定ToolStripMenuItem,
+            this.列印ToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.結束XToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(75, 22);
+            this.toolStripDropDownButton1.Text = "檔案(F)";
+            this.toolStripDropDownButton1.Click += new System.EventHandler(this.toolStripDropDownButton1_Click);
+            // 
+            // 開啟新檔ToolStripMenuItem
+            // 
+            this.開啟新檔ToolStripMenuItem.Name = "開啟新檔ToolStripMenuItem";
+            this.開啟新檔ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.開啟新檔ToolStripMenuItem.Text = "開啟新檔(N)";
+            this.開啟新檔ToolStripMenuItem.Click += new System.EventHandler(this.開啟新檔ToolStripMenuItem_Click);
+            // 
+            // 開啟舊檔ToolStripMenuItem
+            // 
+            this.開啟舊檔ToolStripMenuItem.Name = "開啟舊檔ToolStripMenuItem";
+            this.開啟舊檔ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.開啟舊檔ToolStripMenuItem.Text = "開啟舊檔(O)";
+            // 
+            // 儲存檔案SToolStripMenuItem
+            // 
+            this.儲存檔案SToolStripMenuItem.Name = "儲存檔案SToolStripMenuItem";
+            this.儲存檔案SToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.儲存檔案SToolStripMenuItem.Text = "儲存檔案(S)";
+            // 
+            // 另存新檔ToolStripMenuItem
+            // 
+            this.另存新檔ToolStripMenuItem.Name = "另存新檔ToolStripMenuItem";
+            this.另存新檔ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.另存新檔ToolStripMenuItem.Text = "另存新檔";
+            // 
+            // 版面設定ToolStripMenuItem
+            // 
+            this.版面設定ToolStripMenuItem.Name = "版面設定ToolStripMenuItem";
+            this.版面設定ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.版面設定ToolStripMenuItem.Text = "版面設定";
+            // 
+            // 列印ToolStripMenuItem
+            // 
+            this.列印ToolStripMenuItem.Name = "列印ToolStripMenuItem";
+            this.列印ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.列印ToolStripMenuItem.Text = "列印";
+            // 
+            // 結束XToolStripMenuItem
+            // 
+            this.結束XToolStripMenuItem.Name = "結束XToolStripMenuItem";
+            this.結束XToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.結束XToolStripMenuItem.Text = "結束(X)";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // toolStripDropDownButton2
+            // 
+            this.toolStripDropDownButton2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5});
+            this.toolStripDropDownButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton2.Image")));
+            this.toolStripDropDownButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton2.Name = "toolStripDropDownButton2";
+            this.toolStripDropDownButton2.Size = new System.Drawing.Size(76, 22);
+            this.toolStripDropDownButton2.Text = "編輯(E)";
+            // 
+            // ToolStripMenuItem
+            // 
+            this.ToolStripMenuItem.Name = "ToolStripMenuItem";
+            this.ToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.ToolStripMenuItem.Text = "復原(U)";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(190, 22);
+            this.toolStripMenuItem1.Text = "剪下(T)";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(190, 22);
+            this.toolStripMenuItem2.Text = "toolStripMenuItem2";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(190, 22);
+            this.toolStripMenuItem3.Text = "toolStripMenuItem3";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(190, 22);
+            this.toolStripMenuItem4.Text = "toolStripMenuItem4";
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(190, 22);
+            this.toolStripMenuItem5.Text = "toolStripMenuItem5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 262);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "記事本-目前開啟檔案：(無)";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem 開啟新檔ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開啟舊檔ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 儲存檔案SToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 另存新檔ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 版面設定ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 列印ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 結束XToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton2;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+    }
+}
+
